9f8565d85647af3cce3761fd9acbea19725fac27
Jordan Cooper <jordan.cooper3000@gmail.com>
2018-04-02 15:52:10 -0500

    [bug] TypeError: 'str' object is not callable

--- crackerjack.py
+++ crackerjack.py
@@ -14,42 +14,40 @@ Options:
     -v --version    Show version.
     -b              Indicates a bcrypt hash password       
     -m --med        Password can be alphanumeric with atleast 1 upper and\or 
-                    lowercase letter. e.g  -m Password123.
-    -d --diff       Password must be of length 8, contain at least 1 upper,
+                    lowercase letter and atleast 6 characters long. e.g  -m Pencil1.
+    -d --diff       Password must be of length 9, contain at least 1 upper,
                     lower alphanumeric character and a special character 
                     (a-z,A-Z,0-9,!@#\$%\^&) e.g -d P@ssw0rd#% .
-    -f --file          Accepts a file with a list of passwords e.g. -f /path/to/password_list.txt.
+    -f --file       Accepts a file with a list of passwords e.g. -f /path/to/password_list.txt.
 
 """
-import logging
-import time
-import json,os,sys
-import random
+import logging,time
+import json,os
 import bcrypt
 from docopt import docopt
 from re import compile
 
-HARD_REGEX = compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])(?=.{8,})')
+#Commonly used regex for accepting passwords across websites forms
+HARD_REGEX = compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])(?=.{9,})')
 MEDIUM_REGEX = compile(r'^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})')
+
+def hard_regex(password): 
+    if HARD_REGEX.match(password): 
+        return True
+    return False
+
+def medium_regex(password): 
+    if MEDIUM_REGEX.match(password): 
+        return True
+    return False
 
 def hash_password(plain_password):
     return bcrypt.hashpw(plain_password,bcrypt.gensalt())
 
-def hard_password(password): 
-    if not HARD_REGEX.match(password): 
-        return False
-    return True
-
-def medium_password(password): 
-    if not MEDIUM_REGEX.match(password): 
-        return False
-    return True
-
 def check_password(password,hashed):
     if bcrypt.checkpw(password,hashed):
-        print "Match Found"
-    else:
-        print "No Matched Found"
+        return True
+    return False
 
 def verify_hash(hash):
     """The prefix "$2a$" or "$2b$" (or "$2y$") 
@@ -73,86 +71,103 @@ def timeit(method):
     return timed
 
 def load_m_words():
-    #load medium regex dic passwords
-    return
+    """Fetches words from a simple word dictionary.
 
-def load_h_words():
-    #load medium regex dic passwords
-    return
-
-def load_e_words(inputfile=None):
-     """Fetches rows from a Bigtable.
-
-    Retrieves rows pertaining to the given keys from the Table instance
-    represented by big_table.  Silly things may happen if
-    other_silly_variable is not None.
-
-    Args:
-        big_table: An open Bigtable Table instance.
-        keys: A sequence of strings representing the key of each table row
-            to fetch.
-        other_silly_variable: Another optional variable, that has a much
-            longer name than the other args, and which does nothing.
+    Files contains dictionary words plus some of the most common passwords 
+    captured from rockyou. No string manipulation occured. 
+    Only for words matching the simplest regular expression. 
 
     Returns:
-        A dict mapping keys to the corresponding table row data
-        fetched. Each row is represented as a tuple of strings. For
-        example:
-
-        {'Serak': ('Rigel VII', 'Preparer'),
-         'Zim': ('Irk', 'Invader'),
-         'Lrrr': ('Omicron Persei 8', 'Emperor')}
-
-        If a key from the keys argument is missing from the dictionary,
-        then that row was not found in the table.
-
-    Raises:
-        IOError: An error occurred accessing the bigtable.Table object.
+        A text file with hashed words from the dictionary
     """
     try:
-        filename = os.path.dirname(sys.argv[0])+"data/words_dictionary.json"
-        with open(filename,"r") as english_dictionary:
-            valid_words = json.load(english_dictionary)
-            #hash dictionary words
-            ff=open("hash_words.txt","a+")
-            for key in valid_words:
-                hash_key=hash_password(key.encode('utf-8'))
-                ff.write(hash_key+"\n")
+        filename = os.getcwd()+"/data/words_dictionary.json"
+        with open(filename,"r") as easy_password_dictionary:
+            valid_words = json.load(easy_password_dictionary)
+            return valid_words
     except Exception as e:
         logging.exception("message")
         print str(e)
     finally:
         ff.close()
 
-def comparepwd():
-    #compare hash passwords with hash dictionary
-    with open("data/hash_words.txt") as h1, open("data/passwords.txt") as h2:
-        words=set(h1.read().split())
-        for line in h2:
-            if line in words:
-                print "Match found: "+line
-                return line
-    print "Match not found"
-    return 0
+def load_h_words():
+    """Fetches words from a simple word dictionary.
 
+    Files contains dictionary words plus some of the most common passwords 
+    captured from rockyou. No string manipulation occured. 
+    Only for words matching the simplest regular expression. 
+
+    Returns:
+        A text file with hashed words from the dictionary
+    """
+    try:
+        filename = os.getcwd()+"/data/words_dictionary.json"
+        with open(filename,"r") as easy_password_dictionary:
+            valid_words = json.load(easy_password_dictionary)
+            return valid_words
+    except Exception as e:
+        logging.exception("message")
+        print str(e)
+    finally:
+        ff.close()
+
+def load_e_words():
+    """Fetches words from a simple word dictionary.
+
+    Files contains dictionary words plus some of the most common passwords 
+    captured from rockyou. No string manipulation occured. 
+    Only for words matching the simplest regular expression. 
+
+    Returns:
+        A text file with hashed words from the dictionary
+    """
+    valid_words=[]
+    filename = os.getcwd()+"/data/samplewords.txt"
+    try:
+        with open(filename,"r") as easy_password_dictionary:
+            for line in easy_password_dictionary:
+                valid_words.append(line.strip('\n'))
+            return valid_words
+    except Exception as e:
+        logging.exception("message")
+        print str(e)
+
+def comparepwd(hash_password,dictionary):
+    """Compare hashed_password with dictionary of passwords
+
+    These words a dictionary words plus some of the most common passwords 
+    captured from rockyou and websters-dictionary. No string manipulation occured. 
+    Only for words matching the simplest regular expression. 
+
+    Returns:
+        If identified returns the plaintext word for the hash else, not found. 
+    """
+    
+    for word in dictionary:
+        if hash_password==hash_password(word):
+            print "Match found , password_hash = "+hash_password+" = "+word
+        else:
+            print "No Match"
+    
 @timeit
 def main(args):
     if args['<plaintext_password>']:
-        load_e_words()
-        plaintext_password=args['<plaintext_password>']
-        if len(plaintext_password)>1:
-            print "List of words"
-        else:
-            entered_pw_hash = hash_password(plaintext_password[0])
-            try:
-                f= open("passwords.txt","a+")
-                f.write(entered_pw_hash+"\n")
-            except Exception as e:
-                logging.exception("message")
-                print str(e)
-            finally:
-                comparepwd()
-                f.close()
+        plaintext_passwords=args['<plaintext_password>']
+        for password in plaintext_passwords:
+            if hard_regex(password):
+                dictionary=load_h_words()
+                entered_pw_hash = hash_password(password)
+                comparepwd(entered_pw_hash,dictionary)
+            elif medium_regex(password):
+                dictionary=load_m_words()
+                entered_pw_hash = hash_password(password)
+                comparepwd(entered_pw_hash,dictionary)
+            else:
+                dictionary=load_e_words()
+                entered_pw_hash = hash_password(password)
+                comparepwd(entered_pw_hash,dictionary)
+
     if args['-b']:
     #hash pasword cracker algorightm
         if args['--med']:
@@ -170,6 +185,5 @@ def main(args):
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='Crackerjack 2.0')
-    print args
     main(args)
  
